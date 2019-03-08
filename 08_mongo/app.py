1ca93ee13ac2b5dee4b11512f8967b2f65f807ce
Angela <atom@stuy.edu>
2019-03-08 03:04:05 -0500

    08

---
+++ 08_mongo/app.py
@@ -0,0 +1,30 @@
+# Cookie -- 
+
+
+from flask import Flask, render_template, session, url_for, redirect, request
+import os
+from util import mongo
+
+app = Flask(__name__) # creates instance of a flask app
+app.secret_key = os.urandom(32) 
+mongo.import_json()
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/ip')
+def ip():
+    ip = request.args["ip"]
+    mongo.changeIP(ip)
+    return redirect(url_for("main"))
+
+@app.route('/search')
+def search():
+    age = request.args["age"]
+    ans = mongo.search_age(age)
+    return render_template("index.html", result = ans)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ 08_mongo/util/mongo.py
@@ -0,0 +1,68 @@
+#Team cookie: Angela Tom & Simon Tsui
+#SoftDev pd7
+#K07 -- Import/Export Bank
+#2019-03-05
+
+import pymongo
+import json
+from pymongo import MongoClient
+
+client = MongoClient()
+SERVER_ADDR = "142.93.207.20"
+connection = MongoClient(SERVER_ADDR)
+db = connection.cookie
+collection = db.population
+
+'''
+The dataset is the United States population in 2010.
+Contents:
+   - females
+   - country
+   - age
+   - males
+   - year
+   - total
+
+Hyperlink to the raw data: http://api.population.io/1.0/population/2010/United%20States/?format=json
+
+Import mechanism: We loaded the json file and inserted it into the collection
+
+'''
+
+# import the json file
+def import_json():
+    with open("data/population.json") as p:
+        j = json.load(p)
+        collection.insert_many(j)
+
+def changeIP(ip):
+    client = MongoClient()
+    SERVER_ADDR = ip
+    connection = MongoClient(SERVER_ADDR)
+    db = connection.cookie
+    collection = db.population        
+
+# search for age
+def search_age(age):
+    a = collection.find({"age": age})
+    for x in a:
+        print (x)
+
+# searches for less than the number given
+def search_females(females):
+    f = collection.find({"females": {"$lt": females}})
+    for x in f:
+        print(x)
+
+# searches for less than the number given
+def search_males(males):
+    m = collection.find({"males": {"$lt": males}})
+    for x in m:
+        print(x)
+
+        
+
+import_json()
+#search_age(0)
+#search_age(3)
+#search_females(50031)
