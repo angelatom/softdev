c160ceb1baf255a7ceac49bd5a6655c07c22c737
Angela <atom@stuy.edu>
2019-04-28 20:53:23 -0400

    most freq?

--- 20_anon-reduce/treasure.py
+++ 20_anon-reduce/treasure.py
@@ -20,6 +20,8 @@ for line in lines:
 def word_freq(s):
     return sum([1 for x in words if x == s.lower()])
 
+print(word_freq("mate"))
+
 #Find total frequency of group of words
 def group_freq(s):
     phrase = s
@@ -27,9 +29,13 @@ def group_freq(s):
     if len(s) == 1:
         return word_freq(s)
     temp = [reduce(lambda x,y: x + ' ' + y, words[i:i + len(s)]) for i in range(len(words) - len(s) + 1)]
-    print(temp)
+    #print(temp)
     return sum([1 for x in temp if x == phrase.lower()])
 
 print(group_freq("one leg"))
 
 #Find most frequently occuring word
+def most_freq():
+    return reduce(lambda x,y: x if word_freq(x) > word_freq(y) else y, words)
+
+print(most_freq())
